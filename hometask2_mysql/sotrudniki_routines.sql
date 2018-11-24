133af6dcc451f92f02fc21683d008141dde3a716
kasianova olesia <kas.olesia@gmail.com>
2018-11-24 13:43:06 +0300

    hometask2

---
+++ hometask2_mysql/sotrudniki_routines.sql
@@ -0,0 +1,121 @@
+-- MySQL dump 10.13  Distrib 8.0.12, for Win64 (x86_64)
+--
+-- Host: 192.168.0.105    Database: sotrudniki
+-- ------------------------------------------------------
+-- Server version	8.0.12
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Temporary view structure for view `max_salary`
+--
+
+DROP TABLE IF EXISTS `max_salary`;
+/*!50001 DROP VIEW IF EXISTS `max_salary`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8mb4;
+/*!50001 CREATE VIEW `max_salary` AS SELECT 
+ 1 AS `lastname`,
+ 1 AS `max`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Temporary view structure for view `sumary_salary`
+--
+
+DROP TABLE IF EXISTS `sumary_salary`;
+/*!50001 DROP VIEW IF EXISTS `sumary_salary`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8mb4;
+/*!50001 CREATE VIEW `sumary_salary` AS SELECT 
+ 1 AS `Название отдела`,
+ 1 AS `Кол-во сотрудников`,
+ 1 AS `Общая Зарплата`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Temporary view structure for view `count_stat`
+--
+
+DROP TABLE IF EXISTS `count_stat`;
+/*!50001 DROP VIEW IF EXISTS `count_stat`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8mb4;
+/*!50001 CREATE VIEW `count_stat` AS SELECT 
+ 1 AS `name`,
+ 1 AS `кол-во сотрудников`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Final view structure for view `max_salary`
+--
+
+/*!50001 DROP VIEW IF EXISTS `max_salary`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`%` SQL SECURITY DEFINER */
+/*!50001 VIEW `max_salary` AS select `stat`.`lastname` AS `lastname`,max(`stat`.`salary`) AS `max` from `stat` where `stat`.`otdel` group by `stat`.`lastname` order by `max` desc */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `sumary_salary`
+--
+
+/*!50001 DROP VIEW IF EXISTS `sumary_salary`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`%` SQL SECURITY DEFINER */
+/*!50001 VIEW `sumary_salary` AS select `deportament`.`name` AS `Название отдела`,count(0) AS `Кол-во сотрудников`,sum(`stat`.`salary`) AS `Общая Зарплата` from (`deportament` left join `stat` on((`stat`.`otdel` = `deportament`.`id_dep`))) group by `deportament`.`id_dep`,`deportament`.`name` */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `count_stat`
+--
+
+/*!50001 DROP VIEW IF EXISTS `count_stat`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`%` SQL SECURITY DEFINER */
+/*!50001 VIEW `count_stat` AS select `deportament`.`name` AS `name`,count(0) AS `кол-во сотрудников` from (`deportament` left join `stat` on((`stat`.`otdel` = `deportament`.`id_dep`))) group by `deportament`.`id_dep`,`deportament`.`name` */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-11-24 13:37:08
